4591dcd2efbca03ba2bac5432f298ecc7740bf4d
Andi <andi.prianto@gmail.com>
2026-07-23 15:20:19 +0700

    porting public pages

---
+++ pages/404.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title> Charity | Teamplate</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <?php
+        require_once __DIR__ . '/../components/head_link.php';
+        ?>
+    </head>
+    <body>
+        <?php
+        require_once __DIR__ . '/../components/preload.php';
+        require_once __DIR__ . '/../components/header.php';
+        ?>
+
+        <main>
+            404
+        </main>
+
+        <?php
+        require_once __DIR__ . '/../components/footer.php';
+        ?>
+
+        <?php
+        require_once __DIR__ . '/../components/body_script.php';
+        ?>
+    </body>
+</html>
